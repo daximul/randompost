fd6a7e8e6339f3ff03219784dc86b0e14b344f84
daximul <79027135+daximul@users.noreply.github.com>
2021-04-12 16:04:35 -0400

    Update murbyrolcom.lua

--- murbyrolcom.lua
+++ murbyrolcom.lua
@@ -1,28 +1,47 @@
 if syn and syn.queue_on_teleport then
 	syn.queue_on_teleport('loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/daximul/randompost/main/murbyrolcom.lua"))();')
 end
+
+local function Serverhop()
+	local x = {}
+	for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+		if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+			x[#x + 1] = v.id
+		end
+	end
+	if #x > 0 then
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+	else
+		Serverhop()
+	end
+end
+
+local function GetRoot()
+	local RootPart = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") or game:GetService("Players").LocalPlayer.Character:FindFirstChild("Torso") or game:GetService("Players").LocalPlayer.Character:FindFirstChild("UpperTorso")
+	return RootPart
+end
+
 loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/daximul/who/main/a/test/what/script.lua")))();
+
 wait(2)
+
 loadstring(game:HttpGetAsync(("https://pastebin.com/raw/XCep3rCU")))();
 local Player = game:GetService("Players").LocalPlayer
 local Mouse = Player:GetMouse()
-Mouse.KeyDown:Connect(function(key)
-	if key:lower() == "m" then
-		-- local TP = game:GetService("TeleportService")
-		-- TP:Teleport(4572253581)
-		execCmd("serverhop")
+
+game:GetService("UserInputService").InputBegan:Connect(function(Key, IsChat)
+	if IsChat then return end
+	if Key.KeyCode == Enum.KeyCode.M then
+		Serverhop()
 	end
-end)
-wait(2)
-Mouse.KeyDown:connect(function(KeyPressed)
-	if KeyPressed:lower() == "x" then
+	if Key.KeyCode == Enum.KeyCode.X then
 		local props = game:GetService("Workspace").Debris.Props:GetDescendants()
 		for i = 1, #props do
 			local prop = props[i]
 			if prop.Name == "Green" then
-				local PlayerCFrame = LocalPlayer.Character.HumanoidRootPart.CFrame
+				local PlayerCFrame = GetRoot().CFrame
 				if prop.Parent.Orientation.Y > -3162 then
-					LocalPlayer.Character.HumanoidRootPart.CFrame = prop.Parent.CFrame + Vector3.new(0, 3, 0)
+					GetRoot().CFrame = prop.Parent.CFrame + Vector3.new(0, 3, 0)
 					wait(0.5)
 					game:GetService("ReplicatedStorage").Events.Loot:FireServer(prop.Parent)
 				end
@@ -30,5 +49,7 @@ Mouse.KeyDown:connect(function(KeyPressed)
 		end
 	end
 end)
+
 wait(2)
+
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
