0349ebfab4a5f5a797c0b7f6f13936cdbc3dbbaa
daximul <79027135+daximul@users.noreply.github.com>
2021-05-01 12:21:20 -0400

    Create random_phrase.lua

---
+++ random_phrase.lua
@@ -0,0 +1,18 @@
+local Phrases = {
+	"DA is literally better than IY",
+	"hot ui",
+	"i cant wait for tohru admin release",
+	"give me synapse now",
+	"how's life?",
+	"if u read this have a good day",
+	"ur cool",
+	"hey vsauce michael here",
+	"hey vsauce michael here, today we are using DA",
+	"i have ascended",
+	"evolution and the Bible are hurting my brain",
+	"where am i?",
+	"ur whitelist key is jk there is none lmaooaeofiair",
+	"save me from this hell hole called furry land",
+	"what",
+}
+return Phrases[math.random(1, #Phrases)]
