e9c1172052012d0630594caf496c9457a71e4d3f
daximul <79027135+daximul@users.noreply.github.com>
2021-05-13 22:10:43 -0400

    Update interesting_fa.lua

--- interesting_fa.lua
+++ interesting_fa.lua
@@ -22,3 +22,44 @@ AddCommand("rejoin", {"rj"}, "rejoins the game you're currently in", {}, functio
         return "Rejoining..."
     end
 end)
+
+
+
+table.filter = function(tbl, ret)
+    if (type(tbl) == "table") then
+        local new = {}
+        for i, v in next, tbl do
+            if (ret(i, v)) then
+                table.insert(new, #new + 1, v)
+            end
+        end
+        return new
+    end
+end
+
+AddCommand("serverhop", {"sh"}, "switches servers (optional: min or max)", {{"min", "max"}}, function(Caller, Args)
+    if (Caller == LocalPlayer) then
+        Utils.Notify(Caller or LocalPlayer, nil, "Looking for servers...");
+
+        local Servers = HttpService:JSONDecode(game:HttpGetAsync(("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=100"):format(game.PlaceId))).data
+        if (#Servers >= 1) then
+            Servers = table.filter(Servers, function(i,v)
+                return v.playing ~= v.maxPlayers and v.id ~= game.JobId
+            end)
+            local Server
+            local Option = Args[1] or ""
+            if (Option:lower() == "min") then
+                Server = Servers[#Servers]
+            elseif (Option:lower() == "max") then
+                Server = Servers[1]
+            else
+                Server = Servers[math.random(1, #Servers)]
+            end
+            syn.queue_on_teleport("loadstring(game:HttpGet(\"https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua\"))()");
+            TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id);
+            return ("joining server (%d/%d players)"):format(Server.playing, Server.maxPlayers);
+        else
+            return "no servers foudn"
+        end
+    end
+end)
