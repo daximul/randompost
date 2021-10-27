7dbe3ef8fa6cfd03dba2ebfdbd288c1dd087da32
daximul <79027135+daximul@users.noreply.github.com>
2021-10-27 00:05:11 -0400

    Update script_table.lua

--- script_table.lua
+++ script_table.lua
@@ -9,12 +9,28 @@ local AddScript = function(scriptname, devs, gameid, scrfunction)
 	}
 end
 
-AddScript("bruh", "your mom", "Universal", function()
-  print("sussy baka mf")
+local LoadURL = function(link)
+	return loadstring(game:HttpGetAsync((tostring(link))))();
+end
+
+AddScript("CMD-X", "Various Developers", "Universal", function()
+  LoadURL("https://raw.githubusercontent.com/Toon-arch/cmdx-1.5/main/minified.lua")
 end)
 
-AddScript("Custom Reset", "Atp Zombie", "17838693", function()
-	Players.LocalPlayer.Character:BreakJoints()
+AddScript("Sense Client", "Toon", "Universal", function()
+  LoadURL("https://raw.githubusercontent.com/Toon-arch/SenseClient/main/main.lua")
+end)
+
+AddScript("Hotel Elephant [Ruin Map]", "Redus", "23578803", function()
+  LoadURL("https://raw.githubusercontent.com/Redusofficial/Redus-Roblox-Scripts/main/Hotel%20elephant.lua")
+end)
+
+AddScript("Saber Simulator [Auto Farm]", "RyanRenolds", "3823781113", function()
+  LoadURL("https://raw.githubusercontent.com/ScriptRUs/Banana/main/SaberSimulatorV2.lua")
+end)
+
+AddScript("Zombie Uprising [Kill Aura]", "MSDOS", "4972091010", function()
+  LoadURL("https://raw.githubusercontent.com/toskaman/fb/master/MSDOS")
 end)
 
 return ScriptsHolder
