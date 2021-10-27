b8320ee032e3d20176df5c70d8fe0421df702f1b
daximul <79027135+daximul@users.noreply.github.com>
2021-10-27 00:06:37 -0400

    Update script_table.lua

--- script_table.lua
+++ script_table.lua
@@ -1,8 +1,8 @@
 local ScriptsHolder = {}
 
 local AddScript = function(scriptname, devs, gameid, scrfunction)
-  ScriptsHolder[#ScriptsHolder + 1] = {
-    ["Name"] = tostring(scriptname),
+	ScriptsHolder[#ScriptsHolder + 1] = {
+		["Name"] = tostring(scriptname),
 		["Dev"] = tostring(devs),
 		["ID"] = tostring(gameid),
 		["Func"] = scrfunction
@@ -14,23 +14,23 @@ local LoadURL = function(link)
 end
 
 AddScript("CMD-X", "Various Developers", "Universal", function()
-  LoadURL("https://raw.githubusercontent.com/Toon-arch/cmdx-1.5/main/minified.lua")
+	LoadURL("https://raw.githubusercontent.com/Toon-arch/cmdx-1.5/main/minified.lua")
 end)
 
 AddScript("Sense Client", "Toon", "Universal", function()
-  LoadURL("https://raw.githubusercontent.com/Toon-arch/SenseClient/main/main.lua")
+	LoadURL("https://raw.githubusercontent.com/Toon-arch/SenseClient/main/main.lua")
 end)
 
 AddScript("Hotel Elephant [Ruin Map]", "Redus", "23578803", function()
-  LoadURL("https://raw.githubusercontent.com/Redusofficial/Redus-Roblox-Scripts/main/Hotel%20elephant.lua")
+	LoadURL("https://raw.githubusercontent.com/Redusofficial/Redus-Roblox-Scripts/main/Hotel%20elephant.lua")
 end)
 
 AddScript("Saber Simulator [Auto Farm]", "RyanRenolds", "3823781113", function()
-  LoadURL("https://raw.githubusercontent.com/ScriptRUs/Banana/main/SaberSimulatorV2.lua")
+	LoadURL("https://raw.githubusercontent.com/ScriptRUs/Banana/main/SaberSimulatorV2.lua")
 end)
 
 AddScript("Zombie Uprising [Kill Aura]", "MSDOS", "4972091010", function()
-  LoadURL("https://raw.githubusercontent.com/toskaman/fb/master/MSDOS")
+	LoadURL("https://raw.githubusercontent.com/toskaman/fb/master/MSDOS")
 end)
 
 return ScriptsHolder
