ebc1bcc568c81c7151f99040820c2f377dd6be25
daximul <79027135+daximul@users.noreply.github.com>
2022-07-04 15:54:59 -0400

    Create country_codes.lua

---
+++ country_codes.lua
@@ -0,0 +1,2 @@
+-- define a table full of country codes
+return {"en-us", "ro", "fr", "de", "es", "it", "pt", "pl", "ru", "ja", "zh-cn", "ko", "nl", "tr", "ar", "cs", "da", "fi", "el", "he", "hu", "id", "ja", "ko", "no", "sv", "th", "vi", "zh-tw"}
